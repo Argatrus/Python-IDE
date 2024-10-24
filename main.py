bff5d8f63f9a2af53786f0b83c3faef98e7f8668
Argatrus <raja.aarav@gmail.com>
2024-10-24 19:35:36 +0530

    Adding save dialogs and making files management easier

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ ctk.set_appearance_mode('Dark')
 ctk.set_default_color_theme('blue')
 
 class Processing:
-    def __init__(self, name='testing.py'):
+    def __init__(self, name='nil'):
         self.codefile = []
         self.name = name
         self.currentcodefile = []
@@ -44,34 +44,16 @@ class ClientSide(ctk.CTk):
         self.main.run(self.textbox.get('1.0', ctk.END))
 
     def save(self):
-        if self.main.name != 'testing':
+        if self.main.name != 'nil':
             self.main.save(self.textbox.get('1.0', ctk.END))
         else:
-            pass
+            self.main = Processing(ctk.filedialog.asksaveasfile().name)
+            self.main.save(self.textbox.get('1.0', ctk.END))
 
     def open(self):
         self.main = Processing(ctk.filedialog.askopenfilename())
         self.textbox.delete('0.0', ctk.END)
         self.textbox.insert('0.0', self.main.show_code())
-
-class OpenFile(ctk.CTk):
-    def __init__(self, m: ClientSide):
-        super().__init__()
-        self.title('Open File')
-        self.geometry('300x100')
-        self.label = ctk.CTkLabel(self, text='Enter Path Below...')
-        self.text = ctk.CTkEntry(self)
-        self.button = ctk.CTkButton(self, text='Press Here to Open File', command=self.path_take)
-        self.label.pack()
-        self.text.pack()
-        self.button.pack()
-        self.m = m
-
-    def path_take(self):
-        self.m.main = Processing(self.text.get())
-        self.m.textbox.delete('0.0', ctk.END)
-        self.m.textbox.insert(self.m.textbox.index(ctk.INSERT), self.m.main.show_code())
-        self.destroy()
 
 class SaveFile(ctk.CTk):
     def __init__(self):
